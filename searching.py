0bf93042bea401e6f1ed88919cce6168e9fb8507
Miroslava Valouskova <257061@vutbr.cz>
2024-04-08 12:34:24 +0200

    zmena

--- searching.py
+++ searching.py
@@ -13,9 +13,23 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    import json
+
+    with open("sequential.json") as f:
+        allowed_keys = json.load(f)["allowed_keys"]
+
+    if field not in allowed_keys:
+        return None
+
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    return data.get(field, None)
+
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print("Sequential Data:", sequential_data)
 
 
 if __name__ == '__main__':
