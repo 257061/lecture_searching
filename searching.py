13842da595a8747b1e0036b3a958466de21d7243
Miroslava Valouskova <257061@vutbr.cz>
2024-04-08 13:10:38 +0200

    nova funkce

--- searching.py
+++ searching.py
@@ -15,21 +15,45 @@ def read_data(file_name, field):
 
     import json
 
-    with open("sequential.json") as f:
-        allowed_keys = json.load(f)["allowed_keys"]
-
-    if field not in allowed_keys:
-        return None
-
-    with open(file_name, "r") as f:
-        data = json.load(f)
-
-    return data.get(field, None)
+    with open("sequential.json", mode="r") as data_file:
+        data = json.load(data_file)
+    for key in data_keys():
+        if field == key:
+            sequential_data = data[field]
+            return sequential_data
+        else:
+            return None
 
 
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print("Sequential Data:", sequential_data)
+
+
+def linear_search(sequence, target):
+
+    positions = []
+    count = 0
+    for i, num in enumerate(sequence):
+         if num == target:
+            positions.append(i)
+            count += 1
+
+    return {"positions": positions, "count": count}
+
+
+def pattern_search(sequence, pattern):
+
+    positions = set()
+    seq_length = len(sequence)
+    pattern_length = len(patterm)
+
+    for i in range(seq_length - pattern_length + 1):
+        if sequence[i:i + pattern_length] == pattern:
+            positions.add((i + pattern_length)/2)
+
+    return positions
+
 
 
 if __name__ == '__main__':
